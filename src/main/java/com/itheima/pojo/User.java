9e99632ea49b29fb28ed4014b2cb6cd9001dea4a
Chance.Lv <2827729525@qq.com>
2020-09-15 18:15:42 +0800

    新增提交

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,27 @@
+package com.itheima.pojo;
+
+/**
+ * @description:
+ * @Author:Chance.L
+ * @Date:2020/9/15 0015 17:01
+ */
+public class User {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
